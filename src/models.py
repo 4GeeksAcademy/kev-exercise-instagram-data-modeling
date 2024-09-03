47cd709bdad355ed05b2424b7aa7df451fb1721f
FloresKev99 <165217315+FloresKev99@users.noreply.github.com>
2024-09-03 17:48:10 +0000

    project done

--- src/models.py
+++ src/models.py
@@ -1,29 +1,56 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+class PostDislike(Base):
+    __tablename__ = 'post_dislike'
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    dislike = Column(Boolean, nullable=False)
+    count = Column(Integer)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
+class PostLike(Base):
+    __tablename__ = 'post_like'
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    post_id = Column(Integer, ForeignKey('post.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    like = Column(Boolean, nullable=False)
+    count = Column(Integer)
+
+class Post(Base):
+    __tablename__ = 'post'
+    id = Column(Integer, primary_key=True)
+    media = Column(String(512), nullable=False)
+    body = Column(String(250), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    comment_id = Column(Integer, ForeignKey('comment.id'))
+
+class Comment(Base):
+    __tablename__ = 'comment'
+    id = Column(Integer, primary_key=True)
+    body = Column(String(1000), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+
+class User(Base):
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    email = Column(String(250), nullable=False)
+    password = Column(String(250), nullable=False)
+
+class FollowRequest(Base):
+    __tablename__ = 'follow_request'
+    id = Column(Integer, primary_key=True)
+    follower_id = Column(Integer, ForeignKey('user.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+    accepted = Column(Boolean, nullable=False)
+
 
     def to_dict(self):
         return {}
